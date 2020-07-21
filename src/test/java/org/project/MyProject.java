7b06b4fb70fec70b24dc424b46c417db27001c30
Aarun2229 <arunuvan@gmail.com>
2020-07-21 08:12:02 -0700

    ktm code entred

--- src/test/java/org/project/MyProject.java
+++ src/test/java/org/project/MyProject.java
@@ -1,5 +1,22 @@
 package org.project;
 
-public class MyProject {
+public class MyProject 
+{
+	public void courseName()
+	{
+		System.out.println("course name is java");
+	}
+	
+	public void studentName()
+	{
+		System.out.println("selvi");
+		
+	}
+	public void rollNum()
+	{
+		System.out.println("007");
+		
+	}
+
 
 }
